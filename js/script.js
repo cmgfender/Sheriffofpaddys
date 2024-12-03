c816122e0977ea835e0ee9990aa78e50125599c1
cmgfender <cmglassmire@gmail.com>
2024-12-03 10:41:14 -0500

    modified:   css/style.css modified:   index.html modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,33 +1,8 @@
-// Dynamic Widget Loading
-const widgets = [
-    {
-        name: "Bluesky",
-        src: "https://bsky.app/embed",
-        title: "Bluesky"
-    },
-    {
-        name: "Goodreads",
-        src: "https://www.goodreads.com/widgets",
-        title: "Goodreads"
-    },
-    {
-        name: "Last.fm",
-        src: "https://www.last.fm/embed",
-        title: "Last.fm"
-    }
-];
+// Mobile Navigation Toggle
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
 
-const widgetContainer = document.getElementById('widget-container');
-
-// Rotate Widgets
-let currentWidgetIndex = 0;
-function loadWidget() {
-    widgetContainer.innerHTML = `
-        <iframe src="${widgets[currentWidgetIndex].src}" title="${widgets[currentWidgetIndex].title}" style="width:100%; height:400px; border:none; border-radius:10px;"></iframe>
-    `;
-    currentWidgetIndex = (currentWidgetIndex + 1) % widgets.length;
-}
-
-// Initial Load
-loadWidget();
-setInterval(loadWidget, 8000); // Rotate every 8 seconds
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+});
